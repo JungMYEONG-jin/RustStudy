5bc0b05986675fd81b7e65634c565f0c9f81ce1a
a60156077 <dwrhksfl77@naver.com>
2023-03-10 12:36:44 +0900

    variable practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let mut vec = vec![1, 2, 3, 4,5 ];
+    let iter = vec.iter();
+    for v in iter{
+        println!("number is {}", v);
+    }
+
+    let mut two = 2;
+    two = two + 1;
+
 }
