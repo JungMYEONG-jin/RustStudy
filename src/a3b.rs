13cac3b1b34e1eb99771f184c69f3d0dc4918e70
a60156077 <dwrhksfl77@naver.com>
2023-03-13 12:21:15 +0900

    function practice
    - use if, else if, else

---
+++ src/a3b.rs
@@ -0,0 +1,9 @@
+pub fn printValueCondition(a: i32) {
+    if a > 5 {
+        println!(">5");
+    } else if a == 5 {
+        println!("==5");
+    } else {
+        println!("<5");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod a1;
 mod a2;
 mod a3;
+mod a3b;
 extern crate core;
 use a1::lastName;
 use crate::a1::printFirstName;
 
 fn main() {
-    let mut mybool = true;
-    a3::displayMessage(mybool);
-    mybool = false;
-    a3::displayMessage(mybool);
+    a3b::printValueCondition(3);
+    a3b::printValueCondition(5);
+    a3b::printValueCondition(6);
 }
 
 
