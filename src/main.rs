6958e904d58204e2968f48d2c08a717360cdeaa3
a60156077 <dwrhksfl77@naver.com>
2023-03-14 12:10:34 +0900

    function practice
    - rust로 코테 풀어보기..

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ mod a3b;
 mod a5;
 extern crate core;
 
+use std::collections::{BTreeMap, HashMap};
+
 fn main() {
-    let mut cnt = 3;
-    a5::printNumberWithLoop(cnt);
+    let str = "codeleet";
+    let indices = vec![4,5,6,7,0,2,1,3];
+    let string = restore_string(str.to_string(), indices);
+    println!("{:?}", string);
 }
 
 
@@ -19,3 +23,16 @@ fn program(a: i32) -> (){
     }
 }
 
+
+pub fn restore_string(s: String, indices: Vec<i32>) -> String {
+    let mut db = BTreeMap::new();
+    let len = s.len() as i32;
+    for i in 0..len{
+        db.insert(indices[i as usize], s.as_bytes()[i as usize] as char);
+    };
+    let mut result = String::new();
+    for key in db.keys(){
+        result.push(*db.get(key).unwrap()); // &char to char
+    };
+    return result;
+}
