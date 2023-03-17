b74868dabd895395f5d8c9331c2bdd1804dd0565
godcoder <dwrhksfl77@naver.com>
2023-03-18 00:37:16 +0900

    메모리 추적  rust는 소유권 모형을 이용해 사용한다.  이동을 안하게 만드려면 대여를 해야한다. & ampersand 사용  데이터는 소유권자만 삭제 가능.

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,30 @@ use std::collections::{BTreeMap, HashMap};
 use crate::a7::Color;
 use crate::a8::{Drink, Flavor};
 
-enum Access{
-    ADMIN,
-    USER,
-    GUEST
+struct Book{
+    pages: i32,
+    rating: i32,
 }
 
 fn main() {
-    let val = 100;
-    let str = val > 100;
-    let result = match  str {
-        true => "big",
-        false => "small"
+    let book = Book{
+        pages: 3,
+        rating: 1,
     };
-    println!("{:?}", result);
+
+    displayCount(&book);
+    displayRating(&book);
 }
+
+fn displayCount(book: &Book) {
+    println!("{:?}", book.pages);
+}
+
+fn displayRating(book: &Book) {
+    println!("{:?}", book.rating);
+}
+
+// 메모리 추적
+// rust는 소유권 모형을 이용해 사용한다.
+// 이동을 안하게 만드려면 대여를 해야한다. & ampersand 사용
+// 데이터는 소유권자만 삭제 가능.
