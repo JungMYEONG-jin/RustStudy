c2c39b69d664675524e348c8c3d75ed14a25d80f
godcoder <dwrhksfl77@naver.com>
2023-03-21 22:14:24 +0900

    옵션 콤비네이터

--- src/main.rs
+++ src/main.rs
@@ -40,84 +40,20 @@ fn findUser(name: &str) -> Option<i32>{
 
 
 fn main() {
-    // closer
-    let mut buffer= String::new();
-    let result = io::stdin().read_line(&mut buffer);
-    if result.is_err() {
-        println!("Error...");
-    }
-    buffer = buffer.trim().to_owned();
-    println!("{:?}", buffer);
-    let user = findUser(buffer.as_str()).map(|id| User{
-        id, name: buffer.to_owned(),
-    });
+    let a: Option<i32> = Some(1);
+    let aSome = a.is_some();
+    dbg!(aSome);
+    let aNone = a.is_none();
+    dbg!(aNone);
+    let aMapped = a.map(|num| num+1);
+    dbg!(aMapped);
+    let aFilter = a.filter(|num| num == &1);
+    dbg!(aFilter);
+    // a가 값이 있으면 a 없다면 closer 값 반환
+    let aElse = a.or_else(|| Some(5));
+    dbg!(aElse);
+    // 값이 없을때 기본값 부여하기에 좋음
+    let unwrapped = a.unwrap_or_else(|| 0);
+    dbg!(unwrapped);
 
-    match user {
-        Some(user) => println!("{:?}", user),
-        None => println!("user not found"),
-    }
-
-}
-
-fn allCaps(word: &str) -> String{
-    word.to_uppercase()
-}
-
-fn clamp(n: i32, lower: i32, upper: i32) -> i32{
-    if n < lower {
-        lower
-    }else if n > upper {
-        upper
-    } else {
-        n
-    }
-}
-
-fn div(a: i32, b: i32) -> Option<i32>{
-    if b == 0 {
-        None
-    }else {
-        Some(a / b)
-    }
-}
-
-
-#[cfg(test)]
-mod test{
-    // crate 소스의 최상단.
-    use crate::*;
-    #[test]
-    fn check_all_caps(){
-        let res = allCaps("hello");
-        let expected = String::from("HELLO");
-        assert_eq!(res, expected, "string should be all uppercase");
-    }
-
-    #[test]
-    fn check_clamp(){
-        let res = clamp(3, 4, 5);
-        let expected = 4;
-        assert_eq!(res, expected, "Should be 4");
-    }
-
-    #[test]
-    fn check_clamp_upper(){
-        let res = clamp(3000, 100, 500);
-        let expected = 500;
-        assert_eq!(res, expected, "Should be 500");
-    }
-
-    #[test]
-    fn div_test(){
-        let res = div(3, 0);
-        let expected = None;
-        assert_eq!(res, expected, "should be 0");
-    }
-
-    #[test]
-    fn div_upper_Zero(){
-        let res = div(0, 3);
-        let expected = 0;
-        assert_eq!(res.unwrap(), expected, "should be 0");
-    }
 }
