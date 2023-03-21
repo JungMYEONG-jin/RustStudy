b0c335fd0ad1ec0b9f5d1a6179a4a2b0f5921ff8
godcoder <dwrhksfl77@naver.com>
2023-03-21 21:41:02 +0900

    Optional에 map 적용하기.

--- src/main.rs
+++ src/main.rs
@@ -22,147 +22,40 @@ use crate::a7::Color;
 use crate::a8::{Drink, Flavor};
 
 
-fn getInput() -> Option<String> {
-    let mut buffer = String::new();
-    while io::stdin().read_line(&mut buffer).is_err(){ // try until success
-        println!("Please retry...");
-    }
-    // trim remove \n
-    let input = buffer.trim().to_owned();
-    if &input == "" {
-        None
-    }else {
-        Some(input)
-    }
+#[derive(Debug)]
+struct User{
+    id: i32,
+    name: String,
 }
 
-fn get_bill_amount() -> Option<f64>{
-    println!("Enter Amount:");
-    loop{
-        let input: String = match getInput() {
-            Some(val) => val,
-            None => "".to_string(),
-        };
-        if &input == "" { // 다시 입력할 기회를 주기 위해 &
-            return None;
-        }
-        let result: Result<f64, _> = input.parse();
-        // _ 오류 무시
-        match result {
-            Ok(amount) => return Some(amount),
-            Err(_) => println!("please type number"),
-        }
+fn findUser(name: &str) -> Option<i32>{
+    let name = name.to_lowercase();
+    match name.as_str() {
+        "sam" => Some(1),
+        "matt" => Some(5),
+        "katie" => Some(9),
+        N_ => None,
     }
 }
-
-fn add_bill_menu(bills: &mut Bills){
-    let name = match getInput() {
-        Some(input) => input,
-        None => return,
-    };
-    let amount = match get_bill_amount() {
-        Some(input) => input,
-        None => return,
-    };
-    let bill = Bill{ name, amount };
-    bills.add(bill);
-    println!("Bill Added");
-}
-
-fn view_bill_menu(bills: &Bills){
-    for bill in bills.getAll(){
-        println!("{:?}", bill);
-    }
-}
-
-fn remove_bill_menu(bills: &mut Bills){
-    for bill in bills.getAll(){
-        println!("{:?}", bill)
-    }
-
-    println!("type name to delete");
-
-    let input = match getInput(){
-        Some(input) => input,
-        None => return,
-    };
-    if bills.remove(&input) {
-        println!("removed");
-    }else {
-        println!("The bill not existed");
-    }
-}
-
-
-fn edit_bill_menu(bills: &mut Bills){
-    for bill in bills.getAll(){
-        println!("{:?}", bill)
-    }
-
-    println!("type name to edit");
-    let input = match getInput(){
-        Some(input) => input,
-        None => return,
-    };
-    let amount = match get_bill_amount(){
-        Some(amount) => amount,
-        None => return,
-    };
-    if bills.edit(&input, amount){
-        println!("updated");
-    }else {
-        println!("The bill not existed");
-    }
-}
-
-fn mainMenu(){
-    fn show(){
-        println!("");
-        println!("Mange Bills");
-        println!("1. Add Bill");
-        println!("2. View Bills");
-        println!("3. Remove Bill");
-        println!("4. Edit Bill");
-        println!("");
-        println!("Enter Selection:");
-    }
-
-    let mut bills = Bills::new();
-    loop{
-        show();
-        let input = match getInput(){
-            Some(input) => input,
-            None => return,
-        };
-        match input.as_str() {
-            "1" => add_bill_menu(&mut bills),
-            "2" => view_bill_menu(&bills),
-            "3" => remove_bill_menu(&mut bills),
-            "4" => edit_bill_menu(&mut bills),
-            _ => break,
-        }
-    }
-}
-
-fn maybe_num() -> Option<i32> {
-
-}
-
-fn maybe_word() -> Option<String> {
-
-}
-
 
 
 fn main() {
     // closer
-    let plus = match maybe_num() {
-        Some(num) => Some(num+1),
-        None => None,
-    };
-    // 위 코드를 아래와 같이 map + 클로저로 해결 가능.
-    // map option이 None일때는 실행안됨.
-    let plus = maybe_num().map(|num| num+1);
-    let wordLenTwice = maybe_word().map(|word| word.len()).map(|len| 2*len);
+    let mut buffer= String::new();
+    let result = io::stdin().read_line(&mut buffer);
+    if result.is_err() {
+        println!("Error...");
+    }
+    buffer = buffer.trim().to_owned();
+    println!("{:?}", buffer);
+    let user = findUser(buffer.as_str()).map(|id| User{
+        id, name: buffer.to_owned(),
+    });
+
+    match user {
+        Some(user) => println!("{:?}", user),
+        None => println!("user not found"),
+    }
+
 }
 
