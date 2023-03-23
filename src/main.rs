20f5dd07fbcfedcbe5fc7608bf625998f2edce57
godcoder <dwrhksfl77@naver.com>
2023-03-23 22:08:06 +0900

    custom error types 연습

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,56 @@ use crate::a8::{Drink, Flavor};
 // use ${lib name 내가 toml에 명시한 lib 이름}
 use demo::{group, print_from_lib};
 
+use thiserror::Error;
 
+#[derive(Debug, Error)]
+enum ProgramError{
+    #[error("menu item not found")]
+    Menu(#[from] MenuError),
+
+    #[error("divide error")]
+    Divide(#[from] MathError),
+}
+
+#[derive(Debug, Error)]
+enum MenuError{
+    #[error("menu item not found")]
+    NotFound,
+}
+
+#[derive(Debug, Error)]
+enum MathError{
+    #[error("dive by zero error")]
+    DivideByZero,
+}
+
+fn pick_menu(choice: &str) -> Result<i32, MenuError>{
+    match choice {
+        "1" => Ok(1),
+        "2" => Ok(2),
+        "3" => Ok(3),
+        _ => Err(MenuError::NotFound),
+    }
+}
+
+fn divide(a: i32, b: i32) -> Result<i32, MathError>{
+    if b!=0 {
+        Ok(a/b)
+    }else {
+        Err(MathError::DivideByZero)
+    }
+}
+
+fn run(step: i32) -> Result<(), ProgramError>
+{
+    if step == 1 {
+        pick_menu("4")?;
+    }else if step == 2 {
+        divide(1, 0)?;
+    }
+    Ok(())
+}
 fn main() {
-    print_from_lib();
-    group::g1::helloG1();
+    println!("{:?}", run(1));
+    println!("{:?}", run(2));
 }
